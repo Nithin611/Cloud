10656cbf633f7e5f5b5673e2440322f3590ec357
Nithin611 <113677170+Nithin611@users.noreply.github.com>
2023-12-04 17:24:30 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 define('DB_SERVER','shopping611.mysql.database.azure.com');
 define('DB_USER','nithin');
 define('DB_PASS' ,'pass@1234');
-define('DB_NAME', 'shopping611');
+define('DB_NAME', 'shopping');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
